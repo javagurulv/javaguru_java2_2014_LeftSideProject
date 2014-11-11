9ec7b8677d462fe25a98448d8f2713b01d344e7c
alekmiku <krop@inbox.lv>
2014-11-11 10:56:00 +0200

    TodoItem.jsp init version web.xml - remove Servlet mapping for TodoItem

--- leftside/src/main/java/lv/javaguru/java2/web/mvc/todoItemServlet/TodoItemController.java
+++ leftside/src/main/java/lv/javaguru/java2/web/mvc/todoItemServlet/TodoItemController.java
@@ -1,7 +1,26 @@
 package lv.javaguru.java2.web.mvc.todoItemServlet;
 
+import lv.javaguru.java2.database.TodoItemDAO;
+import lv.javaguru.java2.database.jdbc.TodoItemDAOImpl;
+import lv.javaguru.java2.web.mvc.core.MVCController;
+import lv.javaguru.java2.web.mvc.core.MVCModel;
+import lv.javaguru.java2.web.mvc.core.MVCProcessor;
+import lv.javaguru.java2.web.mvc.core.MVCRequestParameters;
+
 /**
  * Created by alekmiku on 2014.11.11..
  */
-public class TodoItemController {
+
+@MVCController(path = "/todoItem",
+        pageName = "ToDo Item",
+        isVisible = true)
+public class TodoItemController implements MVCProcessor {
+    private static final String DEFAULT_VIEW = "/TodoItem.jsp";
+    private static final TodoItemDAO todoItemDAO = new TodoItemDAOImpl();
+
+
+    @Override
+    public MVCModel processRequest(MVCRequestParameters req) {
+        return new MVCModel(DEFAULT_VIEW, "Object Data from new MVC model in TodoItem Controller");
+    }
 }
---
+++ leftside/src/main/webapp/TodoItem.jsp
@@ -0,0 +1,5 @@
+<jsp:include page="/includes/header.jsp" />
+<%=
+(String)request.getAttribute("model")
+%>
+<jsp:include page="/includes/footer.jsp" />
